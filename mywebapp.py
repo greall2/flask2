17c3b975f21789b51650bcbc009a9d47e8f84bb9
rionagreally <rionagreally@gmail.com>
2016-11-17 16:39:53 +0000

    Fourth Commit

--- mywebapp.py
+++ mywebapp.py
@@ -1,5 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
+import flask as fl
+
+app = fl.Flask(__name__)
 
 #@app.route("/")
 #def hello():
@@ -9,13 +10,17 @@ def route():
 	return app.send_static_file('index.html')
 	
 
-@app.route("/name")
+@app.route('/name')
 def fname():
-	return "Your name is"
+	return 'Your name is'  + fl.request.values["name"]
 
-@app.route("/name/<name>")
-def name(name):
-	return "Your name is " + name
+
+#@app.route("/name/<name>")
+#def name(name):
+#	return "Your name is " + name
+
+
+	
 
 if __name__ == "__main__":
     app.run()
